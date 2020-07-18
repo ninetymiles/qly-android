fefdbb6865e0efacdf8459a172f6eab673cf644c
Liu <5467193+ninetymiles@users.noreply.github.com>
2020-07-18 16:06:54 +0800

    Correct last commit, should use rewind() not reset()

--- app/src/main/java/com/rex/qly/record/OutputCallbackRtmp.java
+++ app/src/main/java/com/rex/qly/record/OutputCallbackRtmp.java
@@ -29,11 +29,11 @@ public class OutputCallbackRtmp implements SurfaceRecorder.OutputCallback {
         mLogger.trace("sps:{} pps:{}", sps.remaining(), pps.remaining());
         if (!sps.isDirect()) {
             sps = ByteBuffer.allocateDirect(sps.remaining()).put(sps);
-            sps.reset();
+            sps.rewind();
         }
         if (!pps.isDirect()) {
             pps = ByteBuffer.allocateDirect(pps.remaining()).put(pps);
-            pps.reset();
+            pps.rewind();
         }
         mRtmp.sendVideoConfig(sps, pps);
     }
